bdd53f463e52f5a9856165a2521fd6f32727644c
Santiago Diaz Muñoz <149324836+Diaz1010@users.noreply.github.com>
2024-02-12 19:50:42 +0000

    2

---
+++ src/js/component/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+function NavBar() {
+  return (
+    <nav class="navbar navbar-expand-lg  bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand text-light ms-5" href="#"><strong>Start bootstrap</strong></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-flex justify-content-end me-5" id="navbarNav">
+      <ul class="navbar-nav ">
+        <li class="nav-item">
+          <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Service</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  );
+}
+export default NavBar;
